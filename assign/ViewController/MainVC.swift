0fe1e749669704fb7c777ca16e7410ab20cb95a2
TaeYoon17 <kim0517@hotmail.com>
2023-08-11 16:52:49 +0900

    Initial Commit

---
+++ assign/ViewController/MainVC.swift
@@ -0,0 +1,15 @@
+//
+//  MainVC.swift
+//  assign
+//
+//  Created by 김태윤 on 2023/08/11.
+//
+
+import Foundation
+import UIKit
+class MainVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
